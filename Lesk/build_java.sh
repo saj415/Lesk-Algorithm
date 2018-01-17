9cab039cd1fd6ff6a25aa7a1df9be71dc73fc74c
Sachin Joshi <30396084+saj415@users.noreply.github.com>
2018-01-16 19:26:27 -0500

    Add files via upload

---
+++ Lesk/build_java.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp jars/stanford-corenlp-3.8.0.jar:jars/edu.mit.jwi_2.4.0.jar::bin/ -d bin/ src/Lesk.java
---
+++ Lesk/run_java.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+	java -cp jars/stanford-corenlp-3.8.0.jar:jars/edu.mit.jwi_2.4.0.jar:../../../../data\ mining/tools/stanford-corenlp-full-2017-06-09/stanford-corenlp-3.8.0-models.jar:bin/ Lesk
